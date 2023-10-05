f7e7aa29915b2d3cb2bcdb7055b45198f139b7c3
extermachraf <ael-kouc@student.1337.ma>
2023-10-05 18:42:49 +0100

    the front of the nav bar is done

--- front/components/ui/NavBar.tsx
+++ front/components/ui/NavBar.tsx
@@ -1,9 +1,72 @@
-import '@/app/(dashboard)/(routes)/dashboard/style.css'
-import {home} from '@/public/🦆 icon _home_.svg'
+// @ts-nocheck
+'use client'
+import Image from 'next/image';
+import { CSSProperties, useState } from 'react';
+
 export default function NavBar() {
+  const [selectedIcon, setSelectedIcon] = useState(null);
+  const [hoveredIcon, setHoveredIcon] = useState(null);
+
+  const containerStyle: CSSProperties = {
+    width: '2.75rem',
+    height: '40.125rem',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  };
+
+  const buttonStyle: CSSProperties = {
+    padding: '10px',
+    border: 'none',
+    background: 'none',
+    cursor: 'pointer',
+    outline: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    opacity: 0.5, // Initial opacity
+    transition: 'opacity 0.2s', // Add a transition for smoother hover effect
+  };
+
+  const handleIconClick = (iconName) => {
+    setSelectedIcon(iconName);
+  };
+
+  const handleIconMouseEnter = (iconName) => {
+    setHoveredIcon(iconName);
+  };
+
+  const handleIconMouseLeave = () => {
+    setHoveredIcon(null);
+  };
+
+  const renderIconButton = (iconName, imageSrc, altText) => {
+    return (
+      <button
+        style={{
+          ...buttonStyle,
+          opacity: selectedIcon === iconName || hoveredIcon === iconName ? 1 : 0.5,
+        }}
+        onClick={() => handleIconClick(iconName)}
+        onMouseEnter={() => handleIconMouseEnter(iconName)}
+        onMouseLeave={handleIconMouseLeave}
+      >
+        <Image
+          src={imageSrc}
+          alt={altText}
+          width={24}
+          height={21}
+        />
+      </button>
+    );
+  };
+
   return (
-      <div className="style w-[2.75rem] h-[40.125rem] rounded-full  ">
-        <home />
-      </div>
-  )
+    <div className="style rounded-full" style={containerStyle}>
+      {renderIconButton('home', 'home.svg', 'home')}
+      {renderIconButton('game', '🦆 icon _medical cross_.svg', 'game')}
+      {renderIconButton('chat', '🦆 icon _chat_.svg', 'chat')}
+      {renderIconButton('settings', '🦆 icon _cog_.svg', 'settings')}
+    </div>
+  );
 }
