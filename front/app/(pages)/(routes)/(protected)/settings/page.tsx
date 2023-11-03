9d3ea71b633fe7cd7921619c09f3674834d8753d
extermachraf <ael-kouc@student.1337.ma>
2023-11-03 23:07:28 +0100

    settings 80%

--- front/app/(pages)/(routes)/(protected)/settings/page.tsx
+++ front/app/(pages)/(routes)/(protected)/settings/page.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-
+import Pic from "@/components/ui/Profile";
+import FillSet from "@/components/ui/FillSet";
 const settings = () => {
 
   return (
-    <div>
-
+    <div className="h-full rounded-lg  font-alfa-slab flex space-x-2">
+      <div className="w-[40%] rounded-lg bg-[#36393E]">
+        <Pic />
+      </div>
+      <div className="w-[60%]">
+        <FillSet/>
+      </div>
     </div>
   );
 };
---
+++ front/components/ui/Profile.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "./button";
+
+const Pic = () => {
+  const image =
+    "https://cdn.intra.42.fr/users/9373f1cfc045b4628c01920b3000a836/ael-kouc.jpg";
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen  text-gray-300">
+      <Avatar className="w-40 h-40 border-4 ">
+        <AvatarImage src={image} />
+        <AvatarFallback>Done</AvatarFallback>
+      </Avatar>
+      <Button className="mt-4 w-40 bg-[#1E2124] hover:bg-gray-600 text-gray-100">
+        Choose Avatar
+      </Button>
+      <Button className="mt-4 w-40 bg-[#1E2124] hover:bg-gray-600 text-gray-100">
+        Upload Image
+      </Button>
+      <Button className="mt-4 w-40 bg-[#1E2124] hover:bg-gray-600 text-gray-100">
+        Enable 2FA
+      </Button>
+      <Button variant="destructive" className="mt-4 w-40">
+        Delete Account
+      </Button>
+    </div>
+  );
+};
+
+export default Pic;
