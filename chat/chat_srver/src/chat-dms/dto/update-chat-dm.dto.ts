ec71d3e22254cb4fa3a58de824a14c53991dff43
Elias <1337isloading@gmail.com>
2023-12-17 18:56:18 +0100

    merging chat

--- chat/chat_srver/src/chat-dms/dto/update-chat-dm.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChatDmDto } from './create-chat-dm.dto';
-
-export class UpdateChatDmDto extends PartialType(CreateChatDmDto) {
-  id: number;
-}
